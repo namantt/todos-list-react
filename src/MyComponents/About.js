3c9438cdd937b5982b3cb2370c4aaa4aad3dc04d
namantt <namanparashar117@gmail.com>
2022-11-08 15:05:05 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const About = () => {
+    let myStyle = {
+        minHeight: "45vh",
+        margin: "40px auto"
+    }
+    return (
+        <div style={myStyle}>
+            This is about component
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque voluptates ex exercitationem, quidem repudiandae sint animi, perferendis asperiores facilis minima hic, fugiat cupiditate quod inventore! Quod id quae sapiente debitis officiis quo aut neque.</p>
+        </div>
+    )
+}
